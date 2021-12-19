6f25110c19f5c39714abe7ae10cb396a0175e5e5
geun-yeong <lucete387@gmail.com>
2021-12-19 21:22:47 +0900

    clean up source code(intent)

--- filterport/operation.c
+++ filterport/operation.c
@@ -44,61 +44,59 @@ MinifltExampleCreatePostRoutine(
 	_In_         FLT_POST_OPERATION_FLAGS flags
 )
 {
-	UNREFERENCED_PARAMETER(callback_data);
+	UNREFERENCED_PARAMETER(flt_object);
 	UNREFERENCED_PARAMETER(completion_context);
 	UNREFERENCED_PARAMETER(flags);
 
 	PFLT_FILE_NAME_INFORMATION name_info = NULL;
 	UNICODE_STRING test_txt_pattern, file_path;
+	NTSTATUS status;
 
-	if (flt_object && flt_object->FileObject) {
-		NTSTATUS status;
-
-		// get a file path
-		status = FltGetFileNameInformation(
-			callback_data, 
-			FLT_FILE_NAME_NORMALIZED | FLT_FILE_NAME_QUERY_DEFAULT,
-			&name_info
-		);
-		if (!NT_SUCCESS(status)) {
-			return FLT_POSTOP_FINISHED_PROCESSING;
-		}
-
-		status = FltParseFileNameInformation(name_info);
-		IF_ERROR(FltParseFileNameInformation, EXIT_OF_CREATE_POST_OPERATION);
-
-		RtlInitUnicodeString(&test_txt_pattern, L"*TEST.TXT");
-		RtlInitUnicodeString(&file_path, name_info->Name.Buffer);
-		
-		if (!FsRtlIsNameInExpression(&test_txt_pattern, &file_path, TRUE, NULL)) {
-			goto EXIT_OF_CREATE_POST_OPERATION;
-		}
-
-		// get the file name from the full path
-		LPWSTR file_name = wcsrchr(name_info->Name.Buffer, L'\\');
-		if (!file_name) {
-			file_name = name_info->Name.Buffer;
-		}
-
-		FLT_TO_USER sent;        RtlZeroMemory(&sent, sizeof(sent));
-		FLT_TO_USER_REPLY reply; RtlZeroMemory(&reply, sizeof(reply));
-		ULONG returned_bytes = 0;
-
-		// send the file path to client
-		// if client reply to block accessing the file,
-		// the minifilter return STATUS_ACCESS_DENIED to filter manager
-		wcscpy_s(sent.path, ARRAYSIZE(sent.path), name_info->Name.Buffer);
-		status = MinifltPortSendMessage(&sent, sizeof(sent), &reply, sizeof(reply), &returned_bytes);
-		if (NT_SUCCESS(status) && returned_bytes > 0 && reply.block) {
-			callback_data->IoStatus.Status = STATUS_ACCESS_DENIED;
-		}
-
-		DbgPrint(
-			"[filterport] " __FUNCTION__ " [%u] Complete to creat/open a file (%ws)\n",
-			PtrToUint(PsGetCurrentProcessId()),
-			file_name
-		);
+	// get a file path
+	status = FltGetFileNameInformation(
+		callback_data, 
+		FLT_FILE_NAME_NORMALIZED | FLT_FILE_NAME_QUERY_DEFAULT,
+		&name_info
+	);
+	if (!NT_SUCCESS(status)) {
+		goto EXIT_OF_CREATE_POST_OPERATION;
 	}
+
+	status = FltParseFileNameInformation(name_info);
+	IF_ERROR(FltParseFileNameInformation, EXIT_OF_CREATE_POST_OPERATION);
+
+	// check that file name has test.txt string
+	RtlInitUnicodeString(&test_txt_pattern, L"*TEST.TXT");
+	RtlInitUnicodeString(&file_path, name_info->Name.Buffer);
+		
+	if (!FsRtlIsNameInExpression(&test_txt_pattern, &file_path, TRUE, NULL)) {
+		goto EXIT_OF_CREATE_POST_OPERATION;
+	}
+
+	// get the file name from the full path
+	LPWSTR file_name = wcsrchr(name_info->Name.Buffer, L'\\');
+	if (!file_name) {
+		file_name = name_info->Name.Buffer;
+	}
+
+	FLT_TO_USER sent;        RtlZeroMemory(&sent, sizeof(sent));
+	FLT_TO_USER_REPLY reply; RtlZeroMemory(&reply, sizeof(reply));
+	ULONG returned_bytes = 0;
+
+	// send the file path to client
+	// if client reply to block accessing the file,
+	// the minifilter return STATUS_ACCESS_DENIED to filter manager
+	wcscpy_s(sent.path, ARRAYSIZE(sent.path), name_info->Name.Buffer);
+	status = MinifltPortSendMessage(&sent, sizeof(sent), &reply, sizeof(reply), &returned_bytes);
+	if (NT_SUCCESS(status) && returned_bytes > 0 && reply.block) {
+		callback_data->IoStatus.Status = STATUS_ACCESS_DENIED;
+	}
+
+	DbgPrint(
+		"[filterport] " __FUNCTION__ " [%u] Complete to creat/open a file (%ws)\n",
+		PtrToUint(PsGetCurrentProcessId()),
+		file_name
+	);
 
 EXIT_OF_CREATE_POST_OPERATION:
 
